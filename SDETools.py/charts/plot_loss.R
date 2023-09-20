6533478f5f26ec1133daa6713da73a2170f82236
Harry Li <harryxcli@gmail.com>
2023-09-20 10:50:23 +0100

    Updating plots

--- SDETools.py/charts/plot_loss.R
+++ SDETools.py/charts/plot_loss.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(latex2exp)
 
 file_path <- "C:/Users/Harry/OneDrive - Imperial College London/Imperial/Project/Python/SDETools.py/charts"
 description1 <- "N=9,a=1,sig=0"
@@ -13,17 +14,25 @@ data1 |>
   ggplot(aes(x = epoch, y = log10(value))) +
   geom_line() + 
   ylab("log10 loss") +
-  theme_classic()
+  labs(title = TeX("Loss function with $\\sigma = 0$")) + 
+  theme_classic() +
+  theme(text = element_text(size = 20, color = "black"),
+        axis.text = element_text(size = 20, color = "black"),
+        plot.title = element_text(hjust = 0.5))
 
 ggsave(file.path(file_path, paste0(description1, "-loss.png")), 
-       width = 7, height = 4, dpi = 300)
+       width = 8, height = 5, dpi = 300)
 
 data2 |>
   ggplot(aes(x = Step, y = log10(Smoothed.value))) +
   geom_line() + 
   xlab("epoch") +
   ylab("log10 loss") +
-  theme_classic()
+  labs(title = TeX("Loss function with $\\sigma = 0.1$")) + 
+  theme_classic() +
+  theme(text = element_text(size = 20, color = "black"),
+        axis.text = element_text(size = 20, color = "black"),
+        plot.title = element_text(hjust = 0.5))
 
 ggsave(file.path(file_path, paste0(description2, "-loss.png")), 
-       width = 7, height = 4, dpi = 300)
+       width = 8, height = 5, dpi = 300)
